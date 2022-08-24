a4a99d0be9741da370ab3f45cebd231434f60a47
Teguhriyadi <ilham.teguh55@gmail.com>
2022-08-24 10:04:41 +0700

    Membagi Tampilan Menjadi Beberapa Partials

--- company-profile-jldw/src/main.js
+++ company-profile-jldw/src/main.js
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import '../public/template/assets/css/bootstrap.min.css'
+import '../public/template/assets/css/boxicon.min.css'
+import '../public/template/assets/css/templatemo.css'
+import '../public/template/assets/css/custom.css'
+
 createApp(App).use(router).mount('#app')
